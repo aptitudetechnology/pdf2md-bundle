eb4c69bfb585500b24e9700aa52dcb0406ea33ee
Christopher Caston <chris@caston.id.au>
2025-07-17 12:55:53 +0800

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 
 module.exports = {
   mode: 'production',
-  entry: './lib/pdf2md.js',
+  entry: './browser-entry.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'pdf2md.bundle.js',
@@ -26,7 +26,14 @@ module.exports = {
       "zlib": require.resolve("browserify-zlib"),
       "https": require.resolve("https-browserify"),
       "http": require.resolve("stream-http"),
-      "vm": require.resolve("vm-browserify")
+      "vm": require.resolve("vm-browserify"),
+      "canvas": false, // Canvas is not available in browser
+      "worker_threads": false,
+      "child_process": false,
+      "process/browser": require.resolve("process/browser")
+    },
+    alias: {
+      'process': 'process/browser'
     }
   },
   plugins: [
@@ -35,7 +42,8 @@ module.exports = {
       process: 'process/browser',
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      'global': 'globalThis'
     })
   ],
   module: {
@@ -49,11 +57,18 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.wasm$/,
+        type: 'webassembly/async'
       }
     ]
   },
+  experiments: {
+    asyncWebAssembly: true
+  },
   externals: {
-    // If pdf.js is used externally, you might want to externalize it
+    // If you're using pdf.js externally, uncomment this
     // 'pdfjs-dist': 'pdfjsLib'
   }
 };
